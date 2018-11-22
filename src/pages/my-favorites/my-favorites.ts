fd22b300db1cccb1b41c06028ce0c70f9aea0f37
paul <paul.baudrier@epitech.eu>
2018-11-22 23:03:14 +0100

    modif apres crash du pc

--- src/pages/my-favorites/my-favorites.ts
+++ src/pages/my-favorites/my-favorites.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RestProvider } from '../../providers/rest/rest';
 
+import { UserService } from '../core/user.service';
+import { AuthService } from '../core/auth.service';
+import { FirebaseUserModel } from '../core/user.model';
 
 /**
  * Generated class for the MyFavoritesPage page.
@@ -16,17 +19,37 @@ import { RestProvider } from '../../providers/rest/rest';
   templateUrl: 'my-favorites.html',
 })
 export class MyFavoritesPage {
+  user: FirebaseUserModel = new FirebaseUserModel();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public restProvider: RestProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public restProvider: RestProvider, public userService: UserService,
+    public authService: AuthService) {
     this.getUserFavorit();
   }
 
-  users: any;
+  expos: any;
+  getUsers() {
+    this.restProvider.getUsers()
+    .then(data => {
+      this.expos = data;
+
+    console.log(this.expos);
+    });
+  }
+
+  ionViewWillLoad(){
+    this.userService.getCurrentUser()
+    .then(user => {
+      this.user = user;
+      console.log("UID" + user.uid);
+    }, err => console.log(err))
+  }
+
+  favorits: any;
   getUserFavorit() {
     this.restProvider.getUserFavorit()
     .then(data => {
-      this.users = data;
-      console.log(this.users);
+      this.favorits = data;
+      console.log(this.favorits);
     });
   }
 
